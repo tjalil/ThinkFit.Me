cacb16dd36ddcba29c57c6cc861848191a65abb9
Ian Cantor <icantor@fandm.edu>
2014-05-12 15:30:18 -0400

    Defined new, create, user_params, and before filter in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,22 @@
 class UsersController < ApplicationController
+  before_filter: require_login, only: [:index, :show, :edit, :update, :destroy]
+
   def index
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
+
+    if @user.save
+      auto_login(@user) 
+      redirect_to :index
+    else
+      render :new
+    end
   end
 
   def show
@@ -18,5 +29,10 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :name, :height, :weight, :gender)
   end
 end
