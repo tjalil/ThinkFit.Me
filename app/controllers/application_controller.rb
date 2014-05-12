c40226517ba46f74d20d0bf79ce4ff515626d22b
Taha Jalil <taha.jalil@gmail.com>
2014-05-12 16:57:16 -0400

    require user login before accessing pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception 
+
+  private
+
+  def not_authenticated
+    redirect_to new_user_session_path, alert: "Please Log In first"
+  end
 end
