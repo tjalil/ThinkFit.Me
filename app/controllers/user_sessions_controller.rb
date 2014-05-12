9a2294ec000a8acda9fa7f9fdc738c585b581e2a
Taha Jalil <taha.jalil@gmail.com>
2014-05-12 16:52:54 -0400

    created user_sessions controller

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,21 @@
+class UserSessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    if @user = login(params[:email], params[:password])
+      redirect_to user_path(@user), notice: 'Much Success!! Logged in.'
+    else
+      flash.now[:alert] = 'Login failed'
+      render :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to :users, notice: 'Logged out!'
+  end
+
+end
