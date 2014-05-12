4e1d027d12b4b0c7ef19d081cf76493fc1fee68d
Ian Cantor <icantor@fandm.edu>
2014-05-12 15:21:24 -0400

    Created users controller with resources in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Thinkfitme::Application.routes.draw do
+  resources :users
+  
+  root 'users#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
