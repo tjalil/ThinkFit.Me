1400544dab8d02a859f3f9f0be99f47559d1544d
aktoca <axtoca14@gmail.com>
2014-05-12 16:12:54 -0400

    added validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+
+  validates :password, length: {minimum: 6}
+  validates :email, uniqueness: true, presence: true
+  validates_presence_of :name, :gender, :height, :weight 
+
 end
