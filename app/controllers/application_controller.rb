f03b427f1cf26cf18c2ff367a0094fc7ed81f11e
Taha Jalil <taha.jalil@gmail.com>
2014-05-12 23:39:14 -0400

    removed authentication from application controller for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception 
 
-  private
+  # private
 
-  def not_authenticated
-    redirect_to new_user_session_path, alert: "Please Log In first"
-  end
+  # def not_authenticated
+  #   redirect_to new_user_session_path, alert: "Please Log In first"
+  # end
 end
