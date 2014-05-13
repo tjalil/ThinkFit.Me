d54583b33235a75bcadfc65bddb711a8b40b86a1
Ian Cantor <icantor@fandm.edu>
2014-05-13 14:05:42 -0400

    Readded edit and update to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to dashboard_user_path(@user)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def user_params
