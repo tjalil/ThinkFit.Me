01eef15633df02effb9b78e3d62e6d39588e32fd
Taha Jalil <taha.jalil@gmail.com>
2014-05-12 23:41:16 -0400

    edited validations in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
-  validates :password, length: {minimum: 6}
-  validates :email, uniqueness: true, presence: true
   validates_presence_of :name, :gender, :height, :weight 
+  validates_presence_of :email, uniqueness: true
+  validates_presence_of :password, length: { minimum: 6 }
 
   has_many :goals
   has_many :activities, through: :goals
