d732132a7bd2e03f7283ef3ebdb3c5bc8d0cc554
Taha Jalil <taha.jalil@gmail.com>
2014-05-13 15:39:46 -0400

    added validations for presence of fields in activity creation and error message on failure

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -3,8 +3,11 @@ class Goal < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :activity
 	has_many :activity_logs
+
+  validates_presence_of :activity_id, :weekly_goal
 	
   def name_of_activity
     Activity.find(self.activity_id).name
   end
+
 end
