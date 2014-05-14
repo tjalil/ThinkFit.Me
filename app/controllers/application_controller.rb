e57f822488d13f923caa5bbc48afe045fd42f280
Ian Cantor <icantor@fandm.edu>
2014-05-13 21:01:37 -0400

    Created load commentable method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception 
 
-  # private
+  private
 
   # def not_authenticated
   #   redirect_to new_user_session_path, alert: "Please Log In first"
   # end
+
+  def load_commentable
+  	resource, id = request.path.split('/')[1,2]
+  	@commentable = resource.singularize.classify.constantize.find(id)
+  end
 end
