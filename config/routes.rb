3f1b6d381902f201e5a0fa3a00e8a5fe046914a1
Ian Cantor <icantor@fandm.edu>
2014-05-13 21:27:01 -0400

    Edited routes for polymorphic comments

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,16 @@ Thinkfitme::Application.routes.draw do
       get 'dashboard'
     end
 
-    resources :comments, only: [:new, :create, :show]
+    resources :comments, only: [:new, :create]
 
     resources :goals, only: [:new, :create] do
       resources :activity_logs, only: [:new, :create]
     end
     
-    resources :teams, only: [:new, :create, :edit]
+    resources :teams, only: [:new, :create, :edit] do
+      resources :comments, only: [:new, :create]
+    end
+    
   end
   resources :teams, only: [:show]
   resources :user_sessions, only: [:new, :create, :destroy]
