3677d44f061baa415c93ccec4cecb9697bc65538
Ian Cantor <icantor@fandm.edu>
2014-05-13 21:04:29 -0400

    Added commentable polymorphic associations to models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
-	belongs_to :user
+	belongs_to :commentable, polymorphic: true
+
 
 	paginates_per 10
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,4 @@
 class Team < ActiveRecord::Base
   belongs_to :user
+  has_many :comments, as: :commentable
 end
