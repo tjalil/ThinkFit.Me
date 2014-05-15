dbcbcc6e688b0d9a47847909160ae2f872edf856
Taha Jalil <taha.jalil@gmail.com>
2014-05-14 22:07:17 -0400

    created a height_and_weight table for calculating points

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140513191833) do
+ActiveRecord::Schema.define(version: 20140515015908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,14 @@ ActiveRecord::Schema.define(version: 20140513191833) do
     t.integer  "user_id"
     t.integer  "activity_id"
     t.integer  "weekly_goal"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "height_and_weights", force: true do |t|
+    t.decimal  "height",                 precision: 3, scale: 1
+    t.decimal  "ideal_weight_male_kg",   precision: 4, scale: 1
+    t.decimal  "ideal_weight_female_kg", precision: 4, scale: 1
     t.datetime "created_at"
     t.datetime "updated_at"
   end
