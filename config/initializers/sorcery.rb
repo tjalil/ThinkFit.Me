631e96fbf48409076705da6b2297a5477cdf0e60
Ian Cantor <icantor@fandm.edu>
2014-05-18 16:30:42 -0400

    Added facebook app keys and installed Figaro

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -109,8 +109,8 @@ Rails.application.config.sorcery.configure do |config|
   # config.twitter.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=twitter"
   # config.twitter.user_info_mapping = {:email => "screen_name"}
   #
-  # config.facebook.key = ""
-  # config.facebook.secret = ""
+  # config.facebook.key = Figaro.env.facebook_app_id
+  # config.facebook.secret = Figaro.env.facebook_app_secret
   # config.facebook.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=facebook"
   # config.facebook.user_info_mapping = {:email => "name"}
   # config.facebook.access_permissions = ["email", "publish_stream"]
