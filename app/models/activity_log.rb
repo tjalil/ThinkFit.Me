f7b141063e68ab1386d2a83162c5418354dce82e
Taha Jalil <taha.jalil@gmail.com>
2014-05-18 22:11:04 -0400

    moved calculate_points to activity log model, created tota_points method in goal model to display total points for each goal

--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -2,4 +2,20 @@ class ActivityLog < ActiveRecord::Base
 
 	belongs_to :goal
 
+  # this rounds points to the nearest ten
+  def round_points(number)
+    new_num = number/10.0
+    rounded_num = new_num.round
+    multiply_num = rounded_num*10
+  end
+
+  def calculate_points
+    if self.intensity
+      activity_by_type = Activity.find_by(type_of_activity: self.intensity)
+      initial_points = (activity_by_type.met)*(self.duration)
+      final_points = round_points(initial_points)
+      final_points
+    end
+  end
+
 end
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -14,37 +14,6 @@ class Goal < ActiveRecord::Base
     Activity.find(self.activity_id).type_of_activity
   end
 
-  def intensity_of_activity
-    if ActivityLog.find_by_goal_id(self.id) != nil
-      ActivityLog.find_by_goal_id(self.id).intensity
-    else
-      0
-    end
-  end
-
-  def duration_of_activity
-    if ActivityLog.find_by_goal_id(self.id) != nil
-      ActivityLog.find_by_goal_id(self.id).duration
-    else
-      0
-    end
-  end
-
-  def distance_of_activity
-    if ActivityLog.find_by_goal_id(self.id) != nil
-      ActivityLog.find_by_goal_id(self.id).distance
-    else
-      0
-    end
-  end
-
-  # this rounds points to the nearest ten
-  def round_points(number)
-    new_num = number/10.0
-    rounded_num = new_num.round
-    multiply_num = rounded_num*10
-  end
-
   def count_activity_log
     @activity_logs = ActivityLog.all
     activity_log_count ||= 0
@@ -54,39 +23,22 @@ class Goal < ActiveRecord::Base
         activity_log_count += 1
       end
     end
-    
     activity_log_count
   end
 
-  def show_points
-    Activity.where(name: self.name_of_activity_through_goal).each do |activity|
-      # if self.intensity_of_activity == activity.type_of_activity
-      #   activity_met = activity.met.to_f
-      #   first_level_points = (activity_met)*(self.duration_of_activity)
-      #   final_level_points = round_points(first_level_points)
-      #   counter += final_level_points
-      #   return counter
-      # else
-        counter ||= 0
-        activity_met = activity.met.to_f
-        first_level_points = (activity_met)*(self.duration_of_activity)
-        final_level_points = round_points(first_level_points)
-        counter += final_level_points
-        return counter
-      # end
+  def total_points
+    @activity_logs = ActivityLog.all
+    count_points ||= 0
+
+    @activity_logs.each do |activity|
+      if activity.goal_id == self.id
+        count_points += activity.points
+      end
     end
+    count_points 
   end
+
 end
-
-
-      # if name_of_activity_through_goal == "Running"
-      #   if self.intensity_of_activity == activity.type_of_activity
-      #     activity_met = activity.met.to_f
-      #     if self.distance_of_activity
-      #       speed = (activity_met)/self.distance_of_activity
-      #         TAKE THE SPEED AND COMPARE ACROSS ALL SPEED INTERVALS (CONTROL FLOW FOR ALL SPEEDS FOR RUNNING)
-      #         IF SPEED BELONGS TO A PARTICULAR INTERVAL
-      #  
 
 
 
