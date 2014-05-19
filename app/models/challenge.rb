c7879c8428c28ffa2ce3da20f99a0eaf552b39eb
aktoca <axtoca14@gmail.com>
2014-05-19 00:47:22 -0400

    Created associations btw Challenge and team/users - Polymorphic Self-referential FTW

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,2 +1,4 @@
 class Challenge < ActiveRecord::Base
+  belongs_to :challengeable, polymorphic: true
+  belongs_to :defendable, polymorphic: true, class_name: "Challenge"
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -3,5 +3,10 @@ class Team < ActiveRecord::Base
   has_and_belongs_to_many :users
   has_many :comments, as: :commentable
 
+  has_many :challenges, as: :challengeable
+  has_many :opponents, through: :challengeable
+  has_many :defendable, class_name: "Challenge", foreign_key: "oppenet_id"
+  has_many :defenders, through: :defendable, source: :team
+
   mount_uploader :avatar, AvatarUploader
 end
