94f8707469383b792da6fd459b66cc270bd644ed
Taha Jalil <taha.jalil@gmail.com>
2014-05-19 20:41:23 -0400

    added capitalization to team name on save

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,7 +2,14 @@ class Team < ActiveRecord::Base
   belongs_to :owner, foreign_key: 'owner_id', class_name: 'User'
   has_and_belongs_to_many :users
   has_many :comments, as: :commentable
+  before_save :capitalize_name
 
   mount_uploader :avatar, AvatarUploader
+
+  private
+
+  def capitalize_name
+    self.name = self.name.split.map(&:capitalize).join(' ')
+  end
 
 end
