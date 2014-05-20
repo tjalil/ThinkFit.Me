d8bd46fa4b58761a24b4d9bd138c58bcd1adc32c
Ian Cantor <icantor@fandm.edu>
2014-05-20 15:17:24 -0400

    Added edit route to goals

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Thinkfitme::Application.routes.draw do
 
     resources :comments, only: [:new, :create]
 
-    resources :goals, only: [:new, :create] do
+    resources :goals, only: [:new, :create, :edit] do
       resources :activity_logs, only: [:new, :create, :destroy]
     end
 
